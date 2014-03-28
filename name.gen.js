4a20e755685e66d8fa285c1658ef0e59e2160033
Ortu- <jpkirklin@gmail.com>
2014-03-27 23:03:50 -0500

    Initial file commit
    Initial files

---
+++ name.gen.js
@@ -0,0 +1,37 @@
+function Name_Generator(){
+
+	this.getName = function(race, gender){
+		
+		/*
+			Currently supports only the generation of a 'Common' or 'First' name.
+			Does not handle generation of 'Family' or 'Last' names.
+			Names will always include a first part and a last part. 
+			Inclusion of a middle part is random. (Currently 50/50 but a weighted parameter could easily be added)
+		*/
+		
+		//Build the name components:
+		
+		var thisPart;
+		var firstPart = "";
+		var middlePart = "";
+		var lastPart = "";
+		
+		//First Part
+		thisPart = nameSeed[race][gender]["ComFirst"];
+			firstPart = thisPart[Math.floor((Math.random() * thisPart.length))];
+
+		//Middle Part
+		if(Math.floor((Math.random()*2)+1) == 2){
+			thisPart = nameSeed[race][gender]["ComMiddle"];
+				middlePart = thisPart[Math.floor((Math.random() * thisPart.length))];
+		}
+			
+		//Last Part
+		thisPart = nameSeed[race][gender]["ComLast"];
+			lastPart = thisPart[Math.floor((Math.random() * thisPart.length))];
+		
+		
+		return firstPart + middlePart + lastPart;
+	}
+
+}
---
+++ name.seed.js
@@ -0,0 +1,92 @@
+var nameSeed = {
+	"Celish": {
+		"Male": {
+			"ComFirst": ["Al","Cal","Hal","Mal","Kor","Aed","Aer","Er","Ar","Gwid","All","Call","Hall","Mall","Cad","Gwyd","Ald","In","Ind","Mer","Dom","Cul","Cull","Dal","Dav","Dall","Kad","Mad","Mel","Med","Ul","Ull","Uld","Mael","Hael","Kael","Har","Ir","An","Ad","Ed","Id","Od","On","Ond","Or","Os","Ur","El","Eld","Culd","Bul","Bar","Bir","Bor","Bur","Lan","Lon","Loth","Camb","Dan","Can","Han","Ban","Man","Mond","Ver","Var","Vard","Verd","Tam","Tim","Tom"],
+			"ComMiddle": ["ind","and","er","ar","ond","der","dar","al","el","il","yl","all","ell","ill","yll","ah","eh","ih","y","an","on","in","ral","rel","rin","ad","rad","ed","id","yd","wal","wad","daer","a","wol","gar","e","i","ir","mor","mar","gur","did","dir","dur","ae","aer","o","dae","rae","trae"],
+			"ComLast": ["ric","roc","dyn","ion","ryn","on","in","iod","iodd","onn","ionn","rynn","en","ioc","ic","an","ont","rac","doc","oc","tin","ton","din","rin","trin","tric","tryn","troc","sric","sryc","od","odd","onn","ain","dane","ane","ran","dan","wic","wyc","as","ras","uc","air","ar","er","or","ot","ott","vic","vyc","vac","voc"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		},
+		"Female": {
+			"ComFirst": ["Ar","Arr","Er","Err","Ail","Eil","Ain","Ein","An","Ann","Mar","Jan","Jen","Lun","Mas","Mir","Mis","Am","Eus","Lis","Ler","Lyr","Sar","San","Mer","Lin","Lyn","Linn","Lynn","Lan","Lar","Jin","Lir","Cyl","Jenn","Ros","Sir","Ser","Siob","Eun","Sal","Sel","Al","El","Cy","Is","Iss","Ag","Gwen","Gwin","Gwyn","Gin","Cas","Cass","Ly","Caer","Car","Lyl","Lil","Ayr","Air","Eir","Eyr","Aur","Aub","Or","Yv","Yr","Ym","Yd","Id","Dor","Dir","Cam","Cim","Cem","Cym"],
+			"ComMiddle": ["ib","a","ar","av","an","al","as","i","ir","iv","in","il","is","e","er","ev","en","el","es","o","or","ov","on","ol","os","y","yr","yv","yn","yl","ys","ab","eb","yb","ob","li","la","ly","b","ia","ea","ay","ob","ab","eb","liv","lev","ad","ed","id","od","yd","and","ind","end","ra","rin","re","ren","ryn","te","ti","sol","iam"],
+			"ComLast": ["wyn","win","wynn","winn","a","ia","ett","ene","eth","in","ine","il","yl","yn","sa","ese","ess","en","el","ral","esa","isa","et","asa","ara","ira","ea","dra","ell","elle","ley","ly","ril","ra","re","relle","rell","ret","ress","rese","rene","rea","ria","leigh","eigh","rey","ila","illa","ola","olla","oa"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		}		
+	},
+	"Uziiri": {
+		"Male": {
+			"ComFirst": ["Mal","Sal","Al","Sam","Shar","Shal","Sar","Sun","Urd","Am","Rav","Sharr","Sarr","All","Sunn","Uz","Uzz","Urz","Ir","Irz","Irr","Ar","Arr","Ur","Urr","Mil","Sil","Ser","Serr","Sur","Hal","San","Sin","Sirr","Tal","Tab","Yaz","Yar","Yur","Sahl","Sor","Sorr","Ar","Arr","Raj","Hak","Kal","Kahl","Khal","Hum","Jan","Sher","Jin","Ahb","Ab","Ihb","Ib","Ibb","Shi","Shay","Shiy","Sha","Av","Kan","Kahn","Khan","Rahj","Rhaj","Rahv","Rhav","Tahl","Tahk","Talk","Zed","Zedd","Zad","Zadd","Zar","Zarr","Zhar","Zal","Zahl","Zhal","Zem","Zhem","Zhen","Tan","Tahn","Sul","Sull","Suhl","Zah","Mar","Marr","Um","Umm","Za","Tur","Ahm","Uhr"],
+			"ComMiddle": ["is","es","as","ast","est","on","an","un","unn","onn","ann","unt","ant","ent","int","ont","iz","izz","af","ef","if","uf","of","oz","ez","ad","at","et","it","mar","mur","mor","in","inn","att","ett","itt","ray","du","ay","ey","sa","sah","ir","irr","er","err","az","azz","ah","aj","or","ar","arr","ah","ahn","am","ahm","dhu"],
+			"ComLast": ["iir","ik","ir","un","err","iz","on","iiz","iik","irr","aj","u","in","eem","im","ahm","am ","ihm","ek","eek","ehk","id","ij","iij","idd","unn","ihd","ihr","ohm","air","iar","ed","eed","ahd","ad"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		},
+		"Female": {
+			"ComFirst": ["Mal","Mel","Mil","Sar","Hep","Al","Sal","Sel","Sil","Sam","Sun","Sum","Shar","Shal","Hel","Hil","Ser","Sur","Rev","San","Sin","Sir","Sarr","Serr","Sirr","Tal","Tab","Yaz","Yur","Yar","Sahl","Sehl","Sihl","Suhl","Surr","Yazm","Su","Hill","Say","Ar","Arr","Dal","Kal","Kahl","Khal","Har","Sher","Jan","Jin","Ib","Ihb","Ahb","Yah","Shi","Saf","Sif","Shay","Shiy","Im","Ab","Yan","Av","Ahv","Siy","Suy","Sey","Jer","Yam","Man","Min","Hev","Heb","Talk","Tahk","Thal","Zar","Zel","Fal","Fel","Fahl","Zhar","Zhel","Zal","Zahl","Zhal","Tan","Tahn","Than","Sul","Sull","Zah","Zi","Mar","Marr","Mir","Mirr","Za","Tur","Ahl","El","Shaz"],
+			"ComMiddle": ["ik","ir","is","iss","es","ess","as","ass","she","er","iz","on","un","i","ay","ey","iy","ek","izz","azz","az","il","onn","unn","ill","err","irr","ikk","ez","shev","iv","irv","av","it","itt","shav","al","el","all","ell","ahl","ehl","eem","ay","ey","a","e","sa","sah","se","seh","if","iff","iy","iir","y","zeb","zehb","za","af","aff","ev","ahv","ehv","iv","ihv","s","z","v","or","orr","an","ahn"],
+			"ComLast": ["a","yah","ia","ahd","ehd","eh","ah","az","as","yeh","an","ann","in","inn","yan","i","loh","ihn","yn","ahn","yahn","yin","yihn","ian","iah","ara","aza","ahr","ahra","ahz","ahza"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		}
+	},
+	"Duccali": {
+		"Male": {
+			"ComFirst": ["Lu","Lui","Mi","Mio","Gi","Gio","Mer","A","Alu","Mico","Micu","Nico","Nica","Ala","Alo","Ali","Ni","Ale","Mir","Mira","Miro","Luo","Adro","Adri","Ti","Tio","Ri","Rio","Li","Ili","Ilo","Ilu","Sa","So","Su","Si","Mili","Milo","Siri","Siro","Mo","Do","Da","Di","Dio","De","Me","Mo","Mono","Le","La","Sio","Ge","Nio","Miri","Miro"],
+			"ComMiddle": ["crete","ce","ca","ri","vo","va","lo","la","cu","chi","cho","che","to","ta","ti","te","co","creti","creta","se","so","na","ve","lu","ra","ro","no","ni","vi","da","do","di","de","du","re","ne","cio","nto","nta","nte","chio","chia","chie","cretci","ran","lan","van","san","rin","lin","vin","sin","rie","tie","ndu","nda"],
+			"ComLast": ["lo","llo","li","lli","cio","gi","go","gio","to","ti","tio","o","veli","velli","velo","vello","teli","telli","telo","tello","co","vani","vano","rio","cael","caeli","caelo","geli","gelo","gelli","gello","vedo","vedi","teo","no","nio","geno","gono","deno","dono","vi","vio","dici","vici","reli","relli","relo","rello","delo","dello","deli","delli","ccio","soli","solo","gento"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		},
+		"Female": {
+			"ComFirst": ["Lu","Lui","Mi","Mia","Gi","Gia","Mer","A","Ala","Mica","Mice","Nico","Nica","Alu","Alo","Ali","Ni","Ale","Mir","Mira","Miro","Lua","Adra","Adri","Ti","Tia","Ri","Ria","Li","Ili","Ila","Ile","Sa","Si","Mili","Mila","Siri","Sira","Ma","Me","Da","Di","Dia","De","Do","Mo","Mona","Le","La","Sia","Ge","Nia","Miri","Mira"],
+			"ComMiddle": ["crete","ce","ca","ri","vo","va","lo","la","cu","chi","cha","che","to","ta","ti","te","co","se","sa","na","ve","le","ra","re","no","ni","vi","creti","creta","da","de","di","do","du","ne","cia","nto","nta","nte","chio","chia","chie","cretci","ran","lan","van","san","rin","lin","vin","sin","rie","tie","ndu","nda"],
+			"ComLast": ["la","lla","le","lle","cia","ge","ga","gia","ta","te","tia","a","vele","velle","vela","vella","tele","telle","tela","tella","ca","vane","vana","ria","caela","gele","gelle","gela","gella","veda","tea","na","nia","gena","gana","dena","dana","va","via","rele","relle","rela","rella","dela","della","dele","delle","ccia","sola","solle","genta"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		}
+	},
+	"Eleron": {
+		"Male": {
+			"ComFirst": [],
+			"ComMiddle": [],
+			"ComLast": [],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		},
+		"Female": {
+			"ComFirst": [],
+			"ComMiddle": [],
+			"ComLast": [],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		}
+	},	
+	"Xichan": {
+		"Male": {
+			"ComFirst": ["Wa","Wu","Xi","Wan","Wun","Sha","Shu","Shan","Shun","Xun","Xan","Cha","Chu","Chan","Sho","Xo","Cho","O","Hu","Ta'","Tu'","Ou","Ho","Ya","Yu","U","Xho","Xha","Ua","Oa","Ka","Ku","Yan","Yun","La'","Lu'","Lo'","Lao'","Ka'o","Wao","Xao","Shao","Chao","Po'","Pao'","Go","Ga","Gu","Ja","Ju","Yang","Yung","Yat","Yut","Mat","Mut","Ma","Mu'","Qa","Qao","Qo","Tsung","Tsang","Tsun","Tsan","Tso","Tsong","Tsa","Tsu","Zan","Tzun","Lang","Lung","Long","Lon","Lan","Lun","Un","Una","Unu","Uno","Usa","Usu","Uso","Qua","Zho","Zha"],
+			"ComMiddle": ["xi","xan","xa","xu","xun","sha","shu","cha","chu","cu","su","ta","tu","ka","ku","kka","kku","tka","tku","son","e","mat","la","lu","en","go","gu","ga","gan","gun","gon","lan","lun","lon","nu","na","no","nor"],
+			"ComLast": ["chan","chin"," lo"," lu"," lan"," lin","shin","shan","san","sin","ku","ka","kka","kku","su","po","go","pan","pin","toro","chon","lon","han","hin"," tsu"," tzu"," tso"," tzo","mato","to"," 'tao","mao"," 'ta"," 'to","ta"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		},
+		"Female": {
+			"ComFirst": ["We","Wi","Xi","Wen","Xen","Win","She","Shi","Shen","Shin","Chen","Chin","Che","Chi","Te'","Ti'","Ai","Ae","A","Yi","Ye","Xhi","Xhe","Xin","Ue","Ui","Oe","Oi","Ki","Ke","Yin","Yen","Li'","Le'","Ly'","Lei'","Ley","Kei","Chei","Yei","Aei","Tei","Wei","Xei","Pi'","Pei","Gi","Ge","Ji","Je","Ying","Yeng","Yit","Yet","Mit","Met","Mei","Mi","Me","Qi","Qe","Qei","Tsin","Tsen","Tsing","Tseng","Tsei","Zen","Zin","Zei","Ling","Leng","Lin","Len","On","Oni","One","Onei","Ona","Onai","Ose","Osi","Osei","Qui","Zhi","Zhe"],
+			"ComMiddle": ["xi","xe","xen","xin","se","si","she","shi","che","chi","sen","sin","ti","te","ki","ke","kki","kke","tki","tke","a","li","le","mit","an","gi","ge","yi","yei","ya","lin","len","ni","ne","nei","nir","ner"],
+			"ComLast": ["chun","chen"," li"," le"," len","shen","shun"," lun","sun","sen","ko","ki","ke","kki","kke","kko","si","se","pi","pe","pen","pun","tori","hun","hen"," tsi"," tzi"," tse"," tze","ti","te"," 'tei","mei","mi","me","mai","kai"," 'ti"," 'te"],
+			"FamFirst": [],
+			"FamMiddle": [],
+			"FamLast": []			
+		}
+	}	
+};
